e43c8251ab12ad0ae40553f87c07032e0d0f0a21
cowglow <cowglow@gmail.com>
2023-08-06 19:37:09 +0200

    Storybook in docs

--- docs/assets/syntaxhighlighter-4OVWVELQ-cb0a3b6d.js
+++
@@ -1,2 +0,0 @@
-import{S as l,c as n,s as d}from"./index-c1f6d73c.js";import"./iframe-0b0542fd.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./index-c589f95a.js";import"./_commonjsHelpers-de833af9.js";import"./index-be0249cb.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
-//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-cb0a3b6d.js.map
